19e3ef8251623e4e7838d2f6db8de3cdb807041e
Phillip Malboeuf <phil@boeuf.coffee>
2019-01-30 22:41:44 -0500

    main instead of app

--- app/app.tsx
+++ app/app.tsx
@@ -4,15 +4,15 @@ import '../styles/styles.scss'
 import * as ReactDOM from 'react-dom'
 import * as React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { AnonymousCredential } from 'mongodb-stitch-core-sdk'
 
 import { Routes } from './routes'
 import { client } from './clients/stitch'
-import { AnonymousCredential } from 'mongodb-stitch-core-sdk'
 
 client.auth
   .loginWithCredential(new AnonymousCredential())
   .then(credential => 
     ReactDOM.render(<BrowserRouter>
       <Routes />
-    </BrowserRouter>, document.getElementById('app'))
+    </BrowserRouter>, document.getElementById('main'))
   )
