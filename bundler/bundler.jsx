293256fb9da58b1b794ab859fc6ae0249e45b339
Phillip Malboeuf <phil@boeuf.coffee>
2019-03-14 10:07:47 -0400

    bundler styled-components

--- bundler/bundler.jsx
+++ bundler/bundler.jsx
@@ -3,6 +3,7 @@ import Path from 'path'
 
 import React from 'react'
 import ReactDOM from 'react-dom/server'
+import { ServerStyleSheet } from 'styled-components'
 import { StaticRouter } from 'react-router'
 
 import { Routes } from '../app/routes'
@@ -12,22 +13,34 @@ const bundler = new Bundler(entries, {
   contentHash: true,
   minify: true,
   scopeHoist: true,
+  sourceMaps: false,
   target: 'browser',
   watch: false,
   detailedReport: true
 })
 
+
+
 class RoutesPackager extends Packager {
   async addAsset(asset) {
-    await this.dest.write(asset.generated.html.replace('<main id="main"></main>', `
-    <main id="main">
-      ${ReactDOM.renderToString(
-        <StaticRouter location={`/${asset.basename.replace('.html', '').replace('index', '')}`} context={{}}>
-          <Routes />
-        </StaticRouter>
-      )}
-    </main>
-    `))
+    const styles = Array.from(asset.depAssets).find(([key, value])=> key.name === './styles/styles.scss')
+    const sheet = new ServerStyleSheet()
+    const html = ReactDOM.renderToString(sheet.collectStyles(
+      <StaticRouter location={`/${asset.basename.replace('.html', '').replace('index', '')}`} context={{}}>
+        <Routes />
+      </StaticRouter>
+    ))
+
+    await this.dest.write(asset.generated.html
+      .replace('<main id="main"></main>', `
+        <main id="main">
+          ${html}
+        </main>
+      `)
+      .replace(new RegExp('<link rel="stylesheet" href="/styles.*.css">'), `
+        <style>${styles[1].generated.css}</style>
+        ${sheet.getStyleTags()}
+      `))
   }
 }
 
